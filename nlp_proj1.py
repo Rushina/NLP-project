8af5b81d6e501ec7900254a01c23139ae4116eed
rushina <rushina.shah7@gmail.com>
2020-07-07 17:17:47 -0400

    Modifications to nlp file, saving tensorflow model

--- nlp_proj1.py
+++ nlp_proj1.py
@@ -1,19 +1,49 @@
 import random
 import numpy as np
 import tensorflow as tf
-from playsound import playsound
+import os
 
 class Logger(object):
+    def __init__(self, mode='log'):
+        self.mode = 0
+        if (mode == 'error'):
+            self.mode = 2
+        elif (mode == 'warn'):
+            self.mode = 1
+        else:
+            self.mode = 0
     def warn(self, str):
-        print('WARN: ', str)
+        if (self.mode < 2):
+            print('WARN: ', str)
     
     def log(self, str):
-        print('Logging: ', str)
+        if (self.mode < 1):
+            print('Logging: ', str)
 
     def error(self, str):
         print('ERROR: ', str)
 
-logger = Logger()
+    
+def read_question_data(filename, num_fields = 3):
+    f = open(filename, "r")
+    raw_data = f.readlines()
+    f.close()
+    q = list()
+    pos = list()
+    neg = list()
+    for td in raw_data:
+        qi, posi, negi = [], [], []
+        if (num_fields == 3):
+            qi, posi, negi = td.split('\t')
+        elif (num_fields == 4):
+            qi, posi, negi, _ = td.split('\t')
+        q.append(qi)
+        pos.append(posi)
+        neg.append(negi)
+    return (q, pos, neg)
+
+
+logger = Logger('log')
 
 logger.log('Reading in word: to word embedding -- mapping words to vectors...')
 f = open('data_folder/all_corpora_vectors.txt', "r")
@@ -43,22 +73,9 @@ for q in raw_text_tokenized:
 
 logger.log('Reading in training data -- query question ID, similar questions ID (pos), random questions ID (neg)...')
 
-f = open('data_folder/data/train_random.txt', "r")
-raw_train_data = f.readlines()
-f.close()
-
-train_q = list()
-train_pos = list()
-train_neg = list()
-
-for td in raw_train_data:
-    q, pos, neg = td.split('\t')
-    train_q.append(q)
-    train_pos.append(pos)
-    train_neg.append(neg)
+train_q, train_pos, train_neg = read_question_data('data_folder/data/train_random.txt')
 
 logger.log('Processing sentences into a single vector using word embedding...')
-
 
 def word2vec(word):
     if not word in word_embed.keys():
@@ -89,7 +106,6 @@ def question2vec(question, N):
 
 logger.log('Creating Model ...')
 
-
 n = 120 # number of sample questions per query question
 N = 100 # number of words per question
 opveclen = 30
@@ -97,52 +113,62 @@ def average(x):
     return tf.keras.backend.mean(x, axis=-2)
 
 wlen = len(word_embed['the'])
-ip = tf.keras.layers.Input(shape=((n+1), N,wlen)) # query question + sample questions
-avg = tf.keras.layers.Lambda(average)
-l1 = tf.keras.layers.Dense(128, activation='relu')
-l2 = tf.keras.layers.Dense(opveclen)
-out = tf.keras.layers.Flatten()
 
-op = out(l2(l1(avg(ip))))
+def create_model():
+    ip = tf.keras.layers.Input(shape=((n+1), N,wlen)) # query question + sample questions
+    avg = tf.keras.layers.Lambda(average)
+    l1 = tf.keras.layers.Dense(128, activation='relu')
+    l2 = tf.keras.layers.Dense(opveclen)
+    out = tf.keras.layers.Flatten()
 
-model = tf.keras.models.Model(inputs=ip, outputs=op)
+    op = out(l2(l1(avg(ip))))
+
+    model = tf.keras.models.Model(inputs=ip, outputs=op)
+    return model
+
+model = create_model()
 
 logger.log('Model inputs and outputs')
 
-batch_size = 10
-samples = np.zeros((batch_size, n, N, wlen))
-labels = np.zeros((batch_size, n+1, opveclen))
-ques = np.zeros((batch_size, N, wlen))
-
-for i, q in enumerate(train_q):
-    if (i >= batch_size):
-        break
-    j = 0
-    ques[i, :, :] = question2vec(question_id[int(q)], N)
-    for pos in train_pos[i].split():
-        p = int(pos)
-        samples[i, j, :, :] = question2vec(question_id[int(p)], N)
-        labels[i, j, :] = 1
-        j += 1
-        if (j >= n):
-            break
-    for neg in train_neg[i].split():
-        if (j >= n):
-            break
-        ns = int(neg)
-        samples[i, j, :, :] = question2vec(question_id[int(ns)], N)
-        labels[i, j, :] = 0
-        j += 1
-    while (j < n):
-        samples[i, j, :, :] = question2vec(question_id[int(ns)], N)
-        labels[i, j, :] = 0
-        j += 1
-
-labels = labels.reshape((batch_size, (n+1)*opveclen))
-ques = tf.convert_to_tensor(ques.reshape(batch_size, 1, N, wlen), dtype=tf.float32) # batch_size X N X wlen
-samples = tf.convert_to_tensor(samples, dtype=tf.float32) 
-labels = tf.convert_to_tensor(labels, dtype=tf.float32)
-data = tf.concat([ques, samples], axis=1)
+def generate_samples(qset, pos_set, neg_set, batch_inds):
+    batch_size = len(batch_inds) 
+    samples = np.zeros((batch_size, n, N, wlen))
+    labels = np.zeros((batch_size, n+1, opveclen))
+    ques = np.zeros((batch_size, N, wlen))
+    r = 0
+    for i in batch_inds:
+        q = qset[i] 
+        j = 0
+        ques[r, :, :] = question2vec(question_id[int(q)], N)
+        for pos in pos_set[i].split():
+            p = int(pos)
+            samples[r, j, :, :] = question2vec(question_id[int(p)], N)
+            labels[r, j, :] = 1
+            j += 1
+            if (j >= n):
+                break
+        if(j == 0):
+            logger.warn('No positive examples detected for example ' + str(i))
+            r -= 1
+            continue
+        for neg in neg_set[i].split():
+            if (j >= n):
+                break
+            ns = int(neg)
+            samples[r, j, :, :] = question2vec(question_id[int(ns)], N)
+            labels[r, j, :] = 0
+            j += 1
+        while (j < n):
+            samples[r, j, :, :] = question2vec(question_id[int(ns)], N)
+            labels[r, j, :] = 0
+            j += 1
+        r += 1
+    labels = labels.reshape((batch_size, (n+1)*opveclen))
+    ques = tf.convert_to_tensor(ques.reshape(batch_size, 1, N, wlen), dtype=tf.float32) # batch_size X N X wlen
+    samples = tf.convert_to_tensor(samples, dtype=tf.float32) 
+    labels = tf.convert_to_tensor(labels, dtype=tf.float32)
+    data = tf.concat([ques, samples], axis=1)
+    return (data, labels)
 
 def loss_fn(y_true, y_pred):
     fq = []
@@ -154,7 +180,6 @@ def loss_fn(y_true, y_pred):
     s = s2/tf.keras.backend.sqrt(tf.keras.backend.sum(fp*fp, axis = -1))
     delta = 0.01
     labels = tf.reshape(y_true, (-1, (n+1), opveclen))[:, :-1, 0]
-    # logger.log(str(labels))
     diff = list()
     for i in range(s.shape[1]):
         d = s[:, i:i+1] - s + labels[:, i:i+1]*delta
@@ -172,75 +197,48 @@ def loss_fn(y_true, y_pred):
             loss.append(l)
     losst = tf.stack(loss, axis=1)
     ls = tf.keras.backend.max(tf.keras.backend.max(losst, axis=-1), axis=-1)
-    # logger.log('inside loss function, true labels = '+ str(y_true))
-    # logger.log('inside loss function, prediction = ' + str(y_pred))
-    # logger.log('inside loss function, loss vector = ' + str(losst))
-    # logger.log('inside loss function: '+ str(ls))
     return ls
-
 
 model.compile(optimizer='adam',
               loss=loss_fn)
 
-op_old = model.predict(data)
-logger.log(str(op_old[0]))
-# logger.log(str(loss_fn(labels, op_old)))
-
-model.fit(data, labels, epochs=2)
-
-
-
-def fit_model(model, batch_size, epochs):
+def fit_model(model, batch_size, epochs, callbacks=[]):
     inds = range(len(train_q))
     num_iter = int(len(train_q)*epochs/batch_size)
-    # logger.log('Number of iterations = '+ str(num_iter))
     for k in range(num_iter):
         batch_inds = random.sample(inds, batch_size)
         inds = [x for x in inds if x not in batch_inds]
-        samples = np.zeros((batch_size, n, N, wlen))
-        labels = np.zeros((batch_size, n+1, opveclen))
-        ques = np.zeros((batch_size, N, wlen))
-        r = 0
-        for i in batch_inds:
-            q = train_q[i] 
-            j = 0
-            ques[r, :, :] = question2vec(question_id[int(q)], N)
-            for pos in train_pos[i].split():
-                p = int(pos)
-                samples[r, j, :, :] = question2vec(question_id[int(p)], N)
-                labels[r, j, :] = 1
-                j += 1
-                if (j >= n):
-                    break
-            if(j == 0):
-                logger.warn('No positive examples detected for example ' + str(i))
-            for neg in train_neg[i].split():
-                if (j >= n):
-                    break
-                ns = int(neg)
-                samples[r, j, :, :] = question2vec(question_id[int(ns)], N)
-                labels[r, j, :] = 0
-                j += 1
-            while (j < n):
-                samples[r, j, :, :] = question2vec(question_id[int(ns)], N)
-                labels[r, j, :] = 0
-                j += 1
-            # logger.log('Inside fit. r= ' + str(r) + 'labels = '+str(labels[r,:,:]))
-            r += 1
-        # logger.log('Inside model fit. Labels: '+ str(labels))
-        labels = labels.reshape((batch_size, (n+1)*opveclen))
-        ques = tf.convert_to_tensor(ques.reshape(batch_size, 1, N, wlen), dtype=tf.float32) # batch_size X N X wlen
-        samples = tf.convert_to_tensor(samples, dtype=tf.float32) 
-        labels = tf.convert_to_tensor(labels, dtype=tf.float32)
-        data = tf.concat([ques, samples], axis=1)
-        model.fit(data, labels, batch_size=batch_size, epochs=1)
+        data, labels = generate_samples(train_q, train_pos,\
+          train_neg, batch_inds)
+        model.fit(data, labels, batch_size=batch_size, \
+         epochs=1, callbacks=callbacks)
         op = model.predict(data)
     return model
     
-model = fit_model(model, 50, 1)
-# model.fit(data, labels, batch_size=32, epochs=5)
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
-op_new = model.predict(data)
-logger.log('New output: '+ str(op_new[0]))
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
-playsound('doorbell-1.wav')
+load_from_checkpoint = False
+if (load_from_checkpoint):
+    model.load_weights(checkpoint_path)
+
+else:
+    model = fit_model(model, 10, 1, callbacks=[cp_callback])
+
+# !mkdir -p saved_model
+model.save('saved_model/simple_nn1')
+
+def post_process(test_file, model):
+    test_q, test_pos, test_neg = read_question_data(test_file, num_fields=4)
+    batch_inds = range(len(test_q))
+    test_ip, test_labels = generate_samples(test_q, test_pos, test_neg, batch_inds)
+    test_op = model.predict(test_ip)
+    print(test_op.shape)
+    print(len(test_q))
+
+post_process('data_folder/data/test.txt', model)
+
