305b727e2418a6a9d0a1f05643a9fcd2f633b1b8
rushina <rushina.shah7@gmail.com>
2020-07-10 17:12:52 -0400

    Added more options for training (ability to train with a smaller set

--- nlp_proj1.py
+++ nlp_proj1.py
@@ -207,17 +207,31 @@ def fit_model_single_data_point(model, train_q, train_pos, train_neg, batch_ind,
     return model 
 
 def fit_model(model, train_q, train_pos, train_neg,\
- batch_size, epochs, dims, question_id, word_embed, callbacks=[]):
-    inds = range(len(train_q))
-    num_iter = int(len(train_q)*epochs/batch_size)
-    for k in range(num_iter):
-        batch_inds = random.sample(inds, batch_size)
-        inds = [x for x in inds if x not in batch_inds]
-        data, labels, batch_size_curr, _ = generate_samples(train_q, train_pos,\
-          train_neg, batch_inds, dims, question_id, word_embed)
-        model.fit(data, labels, batch_size=batch_size_curr, \
-         epochs=1, callbacks=callbacks)
-        op = model.predict(data)
+ batch_size, epochs, dims, question_id, word_embed, callbacks=[], num_data=[]):
+    loss_fn = loss_fn_wrap(dims)
+    if (num_data == []):
+        num_data = len(train_q)
+    num_iter = int(num_data/batch_size)
+    for e in range(epochs):
+        inds = range(num_data)
+        for k in range(num_iter):
+            batch_inds = [] 
+            if (len(inds) >= batch_size):
+                batch_inds = random.sample(inds, batch_size)
+            elif (len(inds) == 0):
+                break
+            else:
+                batch_inds = inds
+            inds = [x for x in inds if x not in batch_inds]
+            data, labels, batch_size_curr, _ = generate_samples(train_q,\
+             train_pos, train_neg, batch_inds, dims, question_id, word_embed)
+            model.fit(data, labels, batch_size=batch_size_curr, \
+             epochs=1, callbacks=callbacks)
+            op = model.predict(data)
+            curr_loss = tf.keras.backend.mean(loss_fn(labels, op))
+            print("Current loss = ", curr_loss)
+            if ((curr_loss) < 0.05):
+                return model 
     return model
     
 def post_process(test_file, model):
@@ -269,19 +283,23 @@ def main():
 
     load_from_checkpoint = False
     train_with_one_datapoint = False
+    train_with_short_set = False
     if (load_from_checkpoint):
         model.load_weights(checkpoint_path)
     else:
         if train_with_one_datapoint:
             model = fit_model_single_data_point(model, train_q, train_pos, train_neg, epochs=10, batch_ind=1, dims=dims, question_id=question_id, word_embed=word_embed, logger=logger)
         else:
+            num_data = 100
+            if not train_with_short_set:
+                num_data = []
             model = fit_model(model, train_q, train_pos, train_neg, \
-             batch_size=10, epochs=1, dims=dims, \
+             batch_size=10, epochs=10, dims=dims, \
              question_id=question_id, word_embed=word_embed, \
-             callbacks=[cp_callback])
+             callbacks=[cp_callback], num_data = num_data)
 
     # !mkdir -p saved_model
-    # model.save('saved_model/simple_nn1')
+    model.save('saved_model/simple_nn3')
     # post_process('data_folder/data/test.txt', model)
 
 if __name__ == "__main__":
